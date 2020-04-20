e142ecc9cc2691e9872a753f5215062306c8209e
Gabriel Vargas Padilha <gabrielvargaspadilha@gmail.com>
2020-04-20 18:50:33 -0300

    Discovery for Zabbix

---
+++ downdetectorDiscovery.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+import os
+import sys
+import json
+FILE = "/usr/lib/zabbix/externalscripts/downdetectorlist.list"
+
+if not os.path.isfile(FILE):
+    print("File not found")
+    sys.exit()
+
+file = open(FILE, "r")
+
+data = {
+    "data": []
+}
+
+for line in file.readlines():
+    line_data = line.rstrip().split(';')
+    if line_data[0] == '1':
+        dic = {"{#SITE_ID}": line_data[1], "{#SITE_NOME}": line_data[2]}
+        data['data'].append(dic)
+
+print(json.dumps(data))
